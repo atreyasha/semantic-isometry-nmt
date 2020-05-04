0f25a627bdc2f55e43cb86ba21ca9001583a9804
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-05-04 15:28:24 +0200

    *shankar* removal of init.sh

--- init.sh
+++ init.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-read -rep "download and deploy WMT 2017 en-de dev/test dataset(s) [2.3 MB]? (y/n): " ans
-if [ $ans == "y" ]; then
-  # download WMT datasets
-  wget "http://data.statmt.org/wmt17/translation-task/preprocessed/de-en/dev.tgz" -O ./data/dev.tar.gz
-  # unzip training data
-  tar -zxvf ./data/dev.tar.gz -C ./data/raw
-  cd ./data
-  # get dev dataset
-  ln -sf ./raw/newstest2015.tc.de ./dev.truecased.de
-  ln -sf ./raw/newstest2015.tc.en ./dev.truecased.en
-  # get test dataset
-  ln -sf ./raw/newstest2016.tc.de ./test.truecased.de
-  ln -sf ./raw/newstest2016.tc.en ./test.truecased.en
-  # return back to base directory
-  cd ..
-fi
-
 read -rep "download and deploy SCPN models [150 MB]? (y/n): " ans
 if [ $ans == "y" ]; then
   fileid="1AuH1aHrE9maYttuSJz_9eltYOAad8Mfj"

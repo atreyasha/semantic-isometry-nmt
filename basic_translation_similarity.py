95f94e9e6344cd3450139623c2122dabe99eed86
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-06-25 17:09:06 +0200

    Addition of batch functionality

--- basic_translation_similarity.py
+++ basic_translation_similarity.py
@@ -37,26 +37,34 @@ def read_data(file_path: str, drop_first: bool = True) -> List[List[str]]:
 
 
 def main() -> None:
+    batch_size = 100
     en_test = read_data("./data/x-final/en/test_2k.tsv")
     de_test = read_data("./data/x-final/de/test_2k.tsv")
     nmt_en2de = torch.hub.load('pytorch/fairseq',
                                'transformer.wmt19.en-de.single_model')
     dict_store = {}
     # write everything to a dictionary json, keep it simple
-    for i, seg in enumerate(tqdm(en_test)):
-        dict_store[seg[0]] = {
-            "sentence_1": {
-                "en": seg[1],
-                "de_gold": de_test[i][1],
-                "de_translate": nmt_en2de.translate(seg[1])
-            },
-            "sentence_2": {
-                "en": seg[2],
-                "de_gold": de_test[i][2],
-                "de_translate": nmt_en2de.translate(seg[2])
-            },
-            "label": seg[3]
-        }
+    for i in tqdm(range(0, len(en_test[:100]), batch_size)):
+        chunk = en_test[i:i+batch_size]
+        de_chunk = de_test[i:i+batch_size]
+        sentence_1s = [seg[1] for seg in chunk]
+        sentence_2s = [seg[2] for seg in chunk]
+        sentence_1s = nmt_en2de.translate(sentence_1s)
+        sentence_2s = nmt_en2de.translate(sentence_2s)
+        for j, seg in enumerate(chunk):
+            dict_store[seg[0]] = {
+                "sentence_1": {
+                    "en": seg[1],
+                    "de_gold": de_chunk[j][1],
+                    "de_translate": sentence_1s[j]
+                },
+                "sentence_2": {
+                    "en": seg[2],
+                    "de_gold": de_chunk[j][2],
+                    "de_translate": sentence_2s[j]
+                },
+                "label": seg[3]
+            }
     with open("store.json", "w") as f:
         json.dump(dict_store, f, ensure_ascii=False)
 

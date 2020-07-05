22f582b4688f022196ad38e3f954693d274f53ef
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-07-06 01:29:42 +0200

    Modification of preprocess script

--- preprocess_wmt16.sh
+++ preprocess_wmt16.sh
@@ -11,10 +11,10 @@ fi
 # pre-process and make ready for model training
 fairseq-preprocess \
     --source-lang de --target-lang en \
-    --trainpref $TEXT/train.tok.clean.bpe.32000 \
-    --validpref $TEXT/newstest2013.tok.bpe.32000 \
-    --testpref $TEXT/newstest2014.tok.bpe.32000 \
-    --destdir data-bin/wmt16_en_de_bpe32k \
+    --trainpref "$TEXT/train.tok.clean.bpe.32000" \
+    --validpref "$TEXT/newstest2013.tok.bpe.32000" \
+    --testpref "$TEXT/newstest2014.tok.bpe.32000" \
+    --destdir "data/wmt16_en_de_bpe32k/bin" \
     --nwordssrc 32768 --nwordstgt 32768 \
     --joined-dictionary \
     --workers 20

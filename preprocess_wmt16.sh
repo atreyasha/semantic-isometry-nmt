9293e2c65d01db0606b2a83fae50fddb4e6e0e3b
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-07-06 01:26:27 +0200

    Addition of preprocess script

---
+++ preprocess_wmt16.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Script sourced and adapted from https://github.com/pytorch/fairseq/blob/master/examples/scaling_nmt/README.md
+
+# extract text from tarball
+TEXT="./data/wmt16_en_de_bpe32k"
+if [ ! -d "$TEXT" ]; then
+  mkdir -p "$TEXT"
+  tar -xzvf "./data/wmt16_en_de.tar.gz" -C "$TEXT"
+fi
+
+# pre-process and make ready for model training
+fairseq-preprocess \
+    --source-lang de --target-lang en \
+    --trainpref $TEXT/train.tok.clean.bpe.32000 \
+    --validpref $TEXT/newstest2013.tok.bpe.32000 \
+    --testpref $TEXT/newstest2014.tok.bpe.32000 \
+    --destdir data-bin/wmt16_en_de_bpe32k \
+    --nwordssrc 32768 --nwordstgt 32768 \
+    --joined-dictionary \
+    --workers 20

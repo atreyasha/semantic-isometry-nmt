b0a54c0f23bfff20e600a1fd491dfeda46161d47
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-07-06 03:06:27 +0200

    Adding uniqueness to model runs

--- train_wmt16_de_en.sh
+++ train_wmt16_de_en.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 # Script sourced and adapted from https://github.com/pytorch/fairseq/blob/master/examples/scaling_nmt/README.md
 
+unix_epoch=$(date +%s)
+
 # train fairseq model
 fairseq-train \
     ./data/wmt16_en_de_bpe32k/bin \
-    --arch transformer_vaswani_wmt_en_de_big --share-all-embeddings \
+    --arch transformer_wmt_en_de --share-all-embeddings \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
     --dropout 0.3 --weight-decay 0.0 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 3584 \
-    --save-dir ./model_logs/wmt16_de_en \
-    --log-format json
+    --save-dir "./model_logs/transformer_wmt16_de_en_${unix_epoch}" \
+    --tensorboard-logdir "./model_logs/transformer_wmt16_de_en_${unix_epoch}" \
+    --keep-last-epochs 10
 
-# --max-epoch
-# --keep-last-epochs
-
-# TODO clean model training with last N checkpoints saved to prevent overwriting
-# TODO specify maximum epochs to train for some failsafe
 # TODO specify verbose logging for later plots
+# TODO figure out correct tensorboard location for server output

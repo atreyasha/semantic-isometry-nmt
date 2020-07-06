fd588e21beefa642475029c397f6eac8b5361a3c
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-07-07 01:49:17 +0200

    Modified training scripts

--- scripts/evaluate_wmt16_de_en.sh
+++ scripts/evaluate_wmt16_de_en.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 # Script sourced and adapted from https://github.com/pytorch/fairseq/blob/master/examples/scaling_nmt/README.md
 
-# assign function for generation
-generate() {
-  # declare positional arguments
-  local path="$1" subset outfile
-  subset=${2:-"test"}
-  outfile="$(dirname "$path")/$(basename "$path")."$subset".out"
-  # evaluate generations
-  fairseq-generate \
-      data/wmt16_en_de_bpe32k/bin \
-      --path "$path" \
-      --beam 4 --lenpen 0.6 --remove-bpe \
-      --gen-subset "$subset" \
-      --max-tokens 2048 | tee "$outfile"
-}
+# declare variables
+path="$1"
+subset="${2:-test}"
+outfile="$(dirname "$path")/$(basename "$path")."$subset".out"
 
-# generate translations
-generate "$@"
+# process generations
+fairseq-generate \
+    data/wmt16_en_de_bpe32k/bin \
+    --path "$path" \
+    --beam 4 --lenpen 0.6 --remove-bpe \
+    --gen-subset "$subset" \
+    --max-tokens 2048 | tee "$outfile"
--- scripts/train_wmt16_de_en.sh
+++ scripts/train_wmt16_de_en.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 # Script sourced and adapted from https://github.com/pytorch/fairseq/blob/master/examples/scaling_nmt/README.md
 
+# declare variables
 unix_epoch="$(date +%s)"
+arch="${1:-transformer_vaswani_wmt_en_de_big}"
 
 # train fairseq model
 fairseq-train \
     ./data/wmt16_en_de_bpe32k/bin \
-    --arch transformer_wmt_en_de --share-all-embeddings \
+    --arch "$arch" --share-all-embeddings \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
     --dropout 0.3 --weight-decay 0.0 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 3584 \
-    --save-dir "./models/transformer_wmt16_de_en_${unix_epoch}" \
-    --tensorboard-logdir "./models/transformer_wmt16_de_en_${unix_epoch}" \
-    --keep-last-epochs 10
+    --save-dir "./models/${arch}.wmt16.de-en.${unix_epoch}" \
+    --tensorboard-logdir "./models/${arch}.wmt16.de-en.${unix_epoch}" \
+    --max-epoch 5

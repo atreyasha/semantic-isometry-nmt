d328486bbf770c2be980c944a29b4f870298acc3
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-07-06 23:48:25 +0200

    Making evaluating bash script more elegant

--- scripts/evaluate_wmt16_de_en.sh
+++ scripts/evaluate_wmt16_de_en.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # Script sourced and adapted from https://github.com/pytorch/fairseq/blob/master/examples/scaling_nmt/README.md
 
-# declare first positional argument as path
-path="$1"
-
 # assign function for generation
 generate() {
+  # declare first positional argument as path
+  local path="$1"
   # evaluate generations
   fairseq-generate \
       data/wmt16_en_de_bpe32k/bin \

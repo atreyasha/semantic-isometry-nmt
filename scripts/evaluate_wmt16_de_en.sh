d7011bceaaf7ac161658cfbad773a5791a747c18
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-07-07 00:01:33 +0200

    Making evaluating bash script more elegant

--- scripts/evaluate_wmt16_de_en.sh
+++ scripts/evaluate_wmt16_de_en.sh
@@ -4,12 +4,15 @@
 # assign function for generation
 generate() {
   # declare first positional argument as path
-  local path="$1"
+  local path="$1" subset outfile
+  subset=${2:-"test"}
+  outfile="$(dirname "$path")/$(basename "$path")."$subset".out"
   # evaluate generations
   fairseq-generate \
       data/wmt16_en_de_bpe32k/bin \
       --path "$path" \
-      --beam 4 --lenpen 0.6 --remove-bpe
+      --beam 4 --lenpen 0.6 --remove-bpe \
+      --gen-subset "$subset" | tee "$outfile"
 }
 
 # generate translations

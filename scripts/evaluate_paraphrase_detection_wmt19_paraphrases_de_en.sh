4c310d19134f185eed5708931e1312645d134e08
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-07-22 12:23:08 +0200

    New evaluation shell script

---
+++ scripts/evaluate_paraphrase_detection_wmt19_paraphrases_de_en.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Script to evaluate WMT19 translations and paraphrases
+# using pre-trained paraphrase detection model(s)
+set -e
+
+# usage function
+usage() {
+  cat <<EOF
+Usage: evaluate_paraphrase_detection_wmt19_paraphrases_de_en.sh [-h|--help] [glob]
+Conduct evaluation of WMT19 paraphrases using pre-trained paraphrase
+detection models
+
+Optional arguments:
+  -h, --help   Show this help message and exit
+  glob <glob>  Glob for finding input json translations, defaults to
+               "./predictions/*/*.json"
+EOF
+}
+
+# check for help
+check_help() {
+  for arg; do
+    if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]; then
+      usage
+      exit 1
+    fi
+  done
+}
+
+# define function
+evaluate_paraphrase_detection_wmt19_paraphrases_de_en() {
+  local glob="${1:-"./predictions/*/*.json"}"
+  python3 -m src.evaluate_paraphrase_detection_wmt19_paraphrases_de_en --json-glob "$glob"
+}
+
+# execute function
+check_help "$@"
+evaluate_paraphrase_detection_wmt19_paraphrases_de_en "$@"

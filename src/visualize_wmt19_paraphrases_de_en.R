ae99736ffe530334fa88a98e3f36128b50fcb046
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-07-26 13:16:59 +0200

    Modified plotting script for paraphrase detector softmax outputs

--- src/visualize_wmt19_paraphrases_de_en.R
+++ src/visualize_wmt19_paraphrases_de_en.R
@@ -3,37 +3,25 @@
 
 library(tools)
 library(rjson)
+library(fields)
 library(ggplot2)
 library(tikzDevice)
 library(optparse)
 library(ggpointdensity)
-library(viridis)
+library(gridExtra)
 
-every_nth <- function(x, nth, empty = TRUE, inverse = FALSE)
-  # can be used to generate additional ticks
-  # source: https://stackoverflow.com/a/34533473
-{
-  x = format(x)
-  if (!inverse) {
-    if(empty) {
-      x[1:nth == 1] <- ""
-      x
-    } else {
-      x[1:nth != 1]
-    }
-  } else {
-    if(empty) {
-      x[1:nth != 1] <- ""
-      x
-    } else {
-      x[1:nth == 1]
-    }
-  }
+g_legend<-function(a.gplot){
+  # source: https://stackoverflow.com/a/13650878
+  # extract legend from custom ggplot object
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
 }
 
 plot_shallow_metrics <- function(input_glob){
   # internal re-usable plot function
-  internal_plot <- function(custom_breaks){
+  internal_plot <- function(){
     # compute summary statistics
     means <- aggregate(subcollection[c("Source","Target")],
                        by=subcollection[c("model_name", "data_name")], FUN=mean)
@@ -107,15 +95,11 @@ plot_shallow_metrics <- function(input_glob){
             axis.ticks.length=unit(.15, "cm"),
             legend.margin=margin(c(1,5,5,15))) +
       facet_grid(data_name ~ model_name) +
-      scale_color_viridis(name="Density") +
+      ## scale_color_viridis(name="Density") +
+      scale_color_gradientn(colours = tim.colors(24),
+                            name="Density") +
       ylab(paste0("Target"," \\textit{",metric,"} [En]","\n")) +
       xlab(paste0("\n","Source"," \\textit{",metric,"} [De]"))
-    if(!is.null(custom_breaks)){
-      g <- g + scale_x_continuous(breaks = custom_breaks,
-                                  labels = every_nth(custom_breaks, 5, inverse=TRUE))
-      g <- g + scale_y_continuous(breaks = custom_breaks,
-                                  labels = every_nth(custom_breaks, 5, inverse=TRUE))
-    }
     return(g)
   }
   files <- Sys.glob(input_glob)
@@ -133,10 +117,10 @@ plot_shallow_metrics <- function(input_glob){
       data_name <- "WMT19 Test Legacy"
     }
     if (grepl("hub", model_name)){
-      model_name <- "FAIR SOTA Transformer"
+      model_name <- "FAIR WMT19 Transformer"
     }
     else {
-      model_name <- "Local Transformer"
+      model_name <- "Scaling NMT WMT16 Transformer"
     }
     filtered <- cbind(model_name, data_name, filtered)
     return(filtered)
@@ -151,36 +135,129 @@ plot_shallow_metrics <- function(input_glob){
   collection <- rbind(collection, hold_out)
   # first plot with chrf
   metric = "chrF"
-  custom_breaks <- seq(0.00, 1.00, 0.05)
   subcollection <- subset(collection, Type == metric)
+  g <- internal_plot()
   tikz(paste0(metric, ".tex"), width=15, height=10, standAlone = TRUE,
-       packages =  paste0(getOption("tikzLatexPackages"),"\\usepackage{amsmath}\n"))
-  g <- internal_plot(NULL)
+       packages =  paste0(getOption("tikzLatexPackages"),"\\usepackage{amsmath}\n"), engine="luatex")
   print(g)
   dev.off()
-  texi2pdf(paste0(metric,".tex"),clean=TRUE)
+  texi2pdf(paste0(metric,".tex"),clean=TRUE,texi2dvi=Sys.which("lualatex"))
   file.remove(paste0(metric,".tex"))
   file.rename(paste0(metric,".pdf"), paste0("./img/", metric, ".pdf"))
   unlink(paste0(metric,"*png"))
   # second plot with BLEU
   metric = "BLEU"
-  custom_breaks <- seq(0, 100, 5)
   subcollection <- subset(collection, Type == metric)
+  g <- internal_plot()
   tikz(paste0(metric, ".tex"), width=15, height=10, standAlone = TRUE,
-       packages =  paste0(getOption("tikzLatexPackages"),"\\usepackage{amsmath}\n"))
-  g <- internal_plot(NULL)
+       packages =  paste0(getOption("tikzLatexPackages"),"\\usepackage{amsmath}\n"), engine="luatex")
   print(g)
   dev.off()
-  texi2pdf(paste0(metric,".tex"),clean=TRUE)
+  texi2pdf(paste0(metric,".tex"),clean=TRUE,texi2dvi=Sys.which("lualatex"))
   file.remove(paste0(metric,".tex"))
   file.rename(paste0(metric,".pdf"), paste0("./img/", metric, ".pdf"))
   unlink(paste0(metric,"*png"))
 }
 
+plot_paraphrase_detector_outputs <- function(input_glob) {
+  files <- Sys.glob(input_glob)
+  collection <- lapply(1:length(files), function(i){
+    data <- fromJSON(file = files[i])
+    data_name = basename(files[i])
+    model_name = basename(dirname(files[i]))
+    indices <- grep("(bert|xlm)", names(data[[1]]))
+    filtered <- data.frame(do.call(rbind, lapply(data, function(x) {
+      unlist(x[indices])})))
+    if (grepl("ar", data_name)) {
+      data_name <- "WMT19 Test AR"
+    }
+    else {
+      data_name <- "WMT19 Test Legacy"
+    }
+    if (grepl("hub", model_name)){
+      model_name <- "FAIR WMT19 Transformer"
+    }
+    else {
+      model_name <- "Scaling NMT WMT16 Transformer"
+    }
+    filtered <- cbind(model_name, data_name, filtered)
+    return(filtered)
+  })
+  collection <- do.call(rbind, collection)
+  # split up data
+  xlmr_indices <- grep("roberta\\.base", names(collection))
+  xlmr <- collection[xlmr_indices]
+  xlmr["Type"] <- "XLM-R-Base"
+  collection <- collection[-xlmr_indices]
+  xlmr_large_indices <- grep("roberta\\.large", names(collection))
+  xlmr_large <- collection[xlmr_large_indices]
+  xlmr_large["Type"] <- "XLM-R-Large"
+  collection <- collection[-xlmr_large_indices]
+  collection["Type"] <- "Multilingual-BERT-Base"
+  bert_indices <- grep("bert\\.base", names(collection))
+  # rename columns
+  names(collection)[grep("src",names(collection))] <- "Source"
+  names(collection)[grep("translated",names(collection))] <- "Target"
+  names(xlmr)[grep("src",names(xlmr))] <- "Source"
+  names(xlmr)[grep("translated",names(xlmr))] <- "Target"
+  names(xlmr_large)[grep("src",names(xlmr_large))] <- "Source"
+  names(xlmr_large)[grep("translated",names(xlmr_large))] <- "Target"
+  # rbind everything together
+  new_collection <- rbind(collection[-(which(names(collection)
+                                             %in% c("model_name","data_name")))], xlmr, xlmr_large)
+  collection <- cbind(collection[c("model_name", "data_name")], new_collection)
+  # make dummy plot
+  dummy <- data.frame(x = seq(0,1,0.01), y = seq(0,1,0.01))
+  q <- ggplot(data=dummy, aes(x=x, y=y)) + geom_point(aes(color=x)) +
+    scale_color_gradientn(colors = tim.colors(100),
+                          name="Normalized Density") +
+    theme(legend.position = "bottom",
+          legend.box="horizontal",
+          legend.key.width = unit(5, "cm"),
+          text = element_text(size=14)) +
+    guides(colour = guide_colourbar(title.position="left", title.hjust = 0.5,
+                                    title.vjust = 0.8))
+  # get legend of dummy plot
+  mylegend<-g_legend(q)
+  # make actual plot
+  g <- ggplot(data=collection, aes(x=Source, y=Target)) +
+    geom_density_2d_filled(contour_var = "ndensity", binwidth=0.01) +
+    scale_fill_manual(values = tim.colors(100),
+                      name="Density") +
+    facet_grid(data_name ~ model_name + Type) +
+    coord_cartesian(expand = FALSE) +
+    xlab("\nSource Paraphrase Softmax Score [De]") +
+    ylab("Target Paraphrase Softmax Score [En]\n") +
+    theme_bw() +
+    theme(text = element_text(size=14),
+          strip.background = element_blank(),
+          ## legend.key.height = unit(0.01, "cm"),
+          legend.position = "none",
+          strip.text = element_text(face="bold"),
+          panel.grid = element_line(size = 1),
+          axis.ticks.length=unit(.15, "cm"),
+          legend.margin=margin(c(1,5,5,15)))
+  tikz(paste0("paraphrase_detection_results.tex"), width=20, height=8,
+       standAlone = TRUE, engine="luatex")
+  grid.arrange(g, mylegend, nrow=2,heights=c(10, 1))
+  dev.off()
+  texi2pdf("paraphrase_detection_results.tex",clean=TRUE,
+           texi2dvi=Sys.which("lualatex"))
+  file.remove("paraphrase_detection_results.tex")
+  file.rename("paraphrase_detection_results.pdf",
+              "./img/paraphrase_detection_results.pdf")
+  unlink("paraphrase_detection_results*.png")
+  unlink("Rplots.pdf")
+}
+
 # parse command-line arguments
 parser <- OptionParser()
-parser <- add_option(parser, c("-s", "--shallow"), action="store_true",
+parser <- add_option(parser, c("-s", "--shallow-metrics"), action="store_true",
+                     default=FALSE,
                      help="Flag for plotting shallow metrics")
+parser <- add_option(parser, c("-p", "--paraphrase-detector-outputs"),
+                     action="store_true", default=FALSE,
+                     help="Flag for plotting paraphrase detector outputs")
 parser <- add_option(parser, c("-j", "--json-glob"),
                      type="character",
                      default="./predictions/*/*.json",
@@ -188,4 +265,6 @@ parser <- add_option(parser, c("-j", "--json-glob"),
 args <- parse_args(parser)
 if(args$s){
   plot_shallow_metrics(args$j)
+} else if(args$p){
+  plot_paraphrase_detector_outputs(args$j)
 }

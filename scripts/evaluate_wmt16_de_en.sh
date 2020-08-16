4369c0b90a96c51af5a60b8a574231710fd467c5
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-08-16 12:25:13 +0200

    Testing modified workflow for wmt16 evaluation

--- scripts/evaluate_wmt16_de_en.sh
+++ scripts/evaluate_wmt16_de_en.sh
@@ -39,14 +39,14 @@ evaluate_wmt16_de_en() {
     "data/wmt16_en_de_bpe32k/bin" \
     --path "$checkpoint_path" \
     --beam 5 --lenpen 0.6 --remove-bpe \
+    --tokenizer moses \
     --gen-subset "$subset" \
     --max-tokens 3584 | tee "$outfile"
   # detokenize and compute sacrebleu
   grep ^H "$outfile" |
-    sed 's/^H\-//' |
+    sed 's/^D\-//' |
     sort -n -k 1 |
     cut -f 3 |
-    sacremoses detokenize |
     sacrebleu --test-set "wmt14/full" \
       --language-pair "de-en" >"${outfile}.sacrebleu"
 }

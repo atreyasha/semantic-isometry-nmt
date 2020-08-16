b098341d42cf2fcde387b7bae7aeea9f7b63c615
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-08-16 12:17:30 +0200

    Reduce test.sh batch size

--- scripts/test.sh
+++ scripts/test.sh
@@ -41,7 +41,7 @@ evaluate_wmt16_de_en() {
     --source-lang de --target-lang en \
     --bpe fastbpe --bpe-codes "data/wmt16_en_de_bpe32k/bpe.32000" \
     --beam 5 --lenpen 0.6 --remove-bpe \
-    --batch-size 512 --buffer-size 1028 \
+    --batch-size 256 --buffer-size 512 \
     --sacrebleu --tokenizer moses --input ./data/wmt19/*ref | tee "$outfile"
   # TODO add sacrebleu evaluation here as well
   # TODO run both wmt16 and wmt19 evaluations to check new tokenizer

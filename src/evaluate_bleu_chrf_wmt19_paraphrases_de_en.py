aaf8d69d81f3e95ae7913185e5e120a148941549
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-08-26 21:54:37 +0200

    Minor bugfix

--- src/evaluate_bleu_chrf_wmt19_paraphrases_de_en.py
+++ src/evaluate_bleu_chrf_wmt19_paraphrases_de_en.py
@@ -36,28 +36,30 @@ def main() -> None:
             source_para_de = store[key]["sentence_paraphrase"]["source"]
             target_orig_en = store[key]["sentence_original"]["target"]
             target_para_en = store[key]["sentence_paraphrase"]["target"]
-            chrf_source = (sacrebleu.sentence_chrf(source_orig_de,
-                                                   [source_para_de]).score +
-                           sacrebleu.sentence_chrf(source_para_de,
-                                                   [source_orig_de]).score) / 2
-            chrf_target = (sacrebleu.sentence_chrf(target_orig_en,
-                                                   [target_para_en]).score +
-                           sacrebleu.sentence_chrf(target_para_en,
-                                                   [target_orig_en]).score) / 2
-            bleu_source = (sacrebleu.sentence_bleu(source_orig_de,
-                                                   [source_para_de]).score +
-                           sacrebleu.sentence_bleu(source_para_de,
-                                                   [source_orig_de]).score) / 2
-            bleu_target = (sacrebleu.sentence_bleu(target_orig_en,
-                                                   [target_para_en]).score +
-                           sacrebleu.sentence_bleu(target_para_en,
-                                                   [target_orig_en]).score) / 2
-            store[key]["chrf_source"] = chrf_source
-            store[key]["chrf_target"] = chrf_target
-            store[key]["bleu_source"] = bleu_source
-            store[key]["bleu_target"] = bleu_target
-            store[key]["chrf_mean"] = (chrf_source + chrf_target) / 2
-            store[key]["bleu_mean"] = (bleu_source + bleu_target) / 2
+            chrf_bar_source = (sacrebleu.sentence_chrf(
+                source_orig_de, [source_para_de]).score +
+                               sacrebleu.sentence_chrf(
+                                   source_para_de, [source_orig_de]).score) / 2
+            chrf_bar_target = (sacrebleu.sentence_chrf(
+                target_orig_en, [target_para_en]).score +
+                               sacrebleu.sentence_chrf(
+                                   target_para_en, [target_orig_en]).score) / 2
+            bleu_bar_source = (sacrebleu.sentence_bleu(
+                source_orig_de, [source_para_de]).score +
+                               sacrebleu.sentence_bleu(
+                                   source_para_de, [source_orig_de]).score) / 2
+            bleu_bar_target = (sacrebleu.sentence_bleu(
+                target_orig_en, [target_para_en]).score +
+                               sacrebleu.sentence_bleu(
+                                   target_para_en, [target_orig_en]).score) / 2
+            store[key]["chrf_bar_source"] = chrf_bar_source
+            store[key]["chrf_bar_target"] = chrf_bar_target
+            store[key]["bleu_bar_source"] = bleu_bar_source
+            store[key]["bleu_bar_target"] = bleu_bar_target
+            store[key]["chrf_bar_mean"] = (chrf_bar_source +
+                                           chrf_bar_target) / 2
+            store[key]["bleu_bar_mean"] = (bleu_bar_source +
+                                           bleu_bar_target) / 2
         # write back json to disk
         with open(input_file, "w") as f:
             store = json.dump(store, f, ensure_ascii=False)
